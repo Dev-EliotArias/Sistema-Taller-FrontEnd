a870fe3938423b5a2123fccf076de9cc838dd66f
Eliot-Arias <rampagecopilation@gmail.com>
2024-05-31 06:55:43 -0500

    wip:dashboard

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { ECharts, EChartsOption } from 'echarts';
+import { NgxEchartsDirective, provideEcharts } from 'ngx-echarts';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule ,NgxEchartsDirective],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrl: './dashboard.component.css',
+  providers: [
+    provideEcharts(),
+  ]
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
 
+  option: EChartsOption = {
+    xAxis: {
+      type: 'category',
+      data: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sept', 'Oct', 'Nov', 'Dic']
+    },
+    yAxis: {
+      type: 'value'
+    },
+    series: [
+      {
+        data: [35, 40, 25, 10, 15, 10, 30, 70, 50, 55, 25, 45],
+        type: 'bar'
+      }
+    ]
+  };
+
+
+
+  ngOnInit(): void {
+
+  }
 }
